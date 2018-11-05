07aec4775f4ebd3000149be0a5a232ac1d2c1155
Daniel Otero <otero.r.daniel@gmail.com>
2018-11-05 10:10:09 -0500

    05112018

--- MultiPlot.R
+++ MultiPlot.R
@@ -4,6 +4,7 @@ x<-x[c(1:102),]
 
 library(ggplot2)
 library(reshape2)
+library(scales)
 
 g.1<-x[,c(6,12)]
 g.2<-x[,c(6,13,14)]
@@ -26,7 +27,7 @@ MultiplePiePlots<-function(data){
       geom_bar(width = 1, stat = "identity")
     
     pie <- bp + coord_polar("y", start=0)
-    library(scales)
+    
     pie + scale_fill_brewer("Sexo") + theme_void() +
       theme(axis.text.x=element_blank())+
       geom_text(aes(y = value/2 + c(0, cumsum(value)[-length(value)]), 
@@ -61,13 +62,13 @@ MultipleBarPlot_G3(g.3)
 MultipleBarPlot_G4<-function(data){
   for (i in 1:nrow(data)){
     data.1<-as.data.frame(data[i,])
-    colnames(x.4.1)<-c("NIDO","Menores 1 año","Entre 1 y 3 años", "Entre 3 y 6 años")
+    colnames(data.1)<-c("NIDO","Menores 1 año","Entre 1 y 3 años", "Entre 3 y 6 años")
     name<-as.character(data.1$NIDO)
-    library(reshape2)
+    
     dd=melt(data.1, id=c("NIDO"))
     dd<-dd[,-1]
     colnames(dd)<-c("group","value")
-    library(ggplot2)
+    
     p <-ggplot(dd, aes(group, value))
     p +geom_bar(stat = "identity",fill="steelblue")+
       geom_text(aes(label=value), vjust=-0.3, size=6)+
@@ -84,13 +85,13 @@ MultipleBarPlot_G4(g.4)
 MultipleBarPlot_G5<-function(data){
   for (i in 1:nrow(data)){
     data.1<-as.data.frame(data[i,])
-    colnames(x.4.1)<-c("NIDO","Afro","Indígenas", "Blanco/Mestizo")
+    colnames(data.1)<-c("NIDO","Afro","Indígenas", "Blanco/Mestizo")
     name<-as.character(data.1$NIDO)
-    library(reshape2)
+    
     dd=melt(data.1, id=c("NIDO"))
     dd<-dd[,-1]
     colnames(dd)<-c("group","value")
-    library(ggplot2)
+    
     p <-ggplot(dd, aes(group, value))
     p +geom_bar(stat = "identity",fill="steelblue")+
       geom_text(aes(label=value), vjust=-0.3, size=6)+
@@ -107,13 +108,13 @@ MultipleBarPlot_G5(g.5)
 MultipleBarPlot_G6<-function(data){
   for (i in 1:nrow(data)){
     data.1<-as.data.frame(data[i,])
-    colnames(x.4.1)<-c("NIDO","Adolescentes","18-24 años", "<24 años")
+    colnames(data.1)<-c("NIDO","Adolescentes","18-24 años", "<24 años")
     name<-as.character(data.1$NIDO)
-    library(reshape2)
+    
     dd=melt(data.1, id=c("NIDO"))
     dd<-dd[,-1]
     colnames(dd)<-c("group","value")
-    library(ggplot2)
+    
     p <-ggplot(dd, aes(group, value))
     p +geom_bar(stat = "identity",fill="steelblue")+
       geom_text(aes(label=value), vjust=-0.3, size=6)+
